78c94116389658d967fad12faf9aca7de35d235f
Isaac Ojo <hojoisaac@gmail.com>
2024-01-11 22:31:31 +0100

    build: Check out from expo go (expo prebuild)

--- app/(tabs)/about.tsx
+++
@@ -1,9 +0,0 @@
-import { View ,Text} from "react-native";
-
-export default function AboutScreen() {
-  return (
-   <View style={{flex:1, backgroundColor:"transparent"}}>
-    <Text>Search Text</Text>
-   </View>
-  );
-}
--- app/(tabs)/starred.tsx
+++
@@ -1,9 +0,0 @@
-import { View ,Text} from "react-native";
-
-export default function StarredScreen() {
-  return (
-   <View style={{flex:1, backgroundColor:"red"}}>
-    <Text>Search Text</Text>
-   </View>
-  );
-}
